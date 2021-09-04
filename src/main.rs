4328d3742ba274431412723fb21b8f3188fd0c16
Anatoliy Skuratov <anatoliy@pleexy.com>
2021-09-04 09:18:44 +0700

    ch5.1

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,10 @@ fn main() {
 }
 
 fn color(r: Ray) -> Vec3 {
-    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Vec3::new(1.0, 0.0, 0.0)
+    let t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = r.point_at_parameter(t).unit_vector() - Vec3::new(0.0, 0.0, -1.0);
+        return Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5;
     }
     let v = r.direction();
     let unit_direction = v.unit_vector();
@@ -45,11 +47,14 @@ fn color(r: Ray) -> Vec3 {
     Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
 }
 
-fn hit_sphere(center: Vec3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Vec3, radius: f64, r: Ray) -> f64 {
     let oc: Vec3 = r.origin() - center;
     let a = r.direction().dot(r.direction());
     let b = 2.0 * oc.dot(r.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+    return (-b - discriminant.sqrt()) / (2.0 * a);
 }
